aa9185c263994f875958c68b96e82d6b9c230aab
Jack <babybishops@gmail.com>
2019-09-20 21:57:59 +0200

    Move source code to src/ directory

--- src/CommandDescriptor.cpp
+++ src/CommandDescriptor.cpp
@@ -12,47 +12,47 @@
 CommandDescriptor* CommandDescriptor::FromJSON(QJsonObject qJsonObj, QString* errorString)
 {
     CommandDescriptor* cd = new CommandDescriptor();
-    
+
     if (! qJsonObj.contains("templateString") )
     {
         *errorString = "There was no template string.";
         return 0;
     }
-    
+
     if (! qJsonObj.value("templateString").isString())
     {
         *errorString = "The template string value was not a string.";
         return 0;
     }
-    
+
     if (qJsonObj.contains("variables") && (! qJsonObj.value("variables").isArray()))
     {
         *errorString = "The variables array was not an array.";
         return 0;
     }
-    
+
     cd -> templateString = qJsonObj.value("templateString").toString();
     cd -> variableMap = new QMap<QString, VariableDescriptor*>;
-    cd -> variableList = new QList<VariableDescriptor*>;    
+    cd -> variableList = new QList<VariableDescriptor*>;
     QJsonArray variables = qJsonObj.value("variables").toArray();
-    
+
     VariableDescriptor* vd;
     QString variableErrorString;
     for (int i = 0; i < variables.count(); i++)
     {
         vd = VariableDescriptor::FromJSON(variables.at(i).toObject(), &variableErrorString);
-        
+
         if (vd == 0)
         {
             *errorString = "There was a problem parsing variable number " + QString::number(i + 1) + " of your variable list. ";
             *errorString += variableErrorString;
             return 0;
         }
-        
+
         cd -> variableMap -> insert(*(vd -> name), vd);
         cd -> variableList -> append(vd);
     }
-    
+
     return cd;
 }
 
@@ -69,45 +69,45 @@ QMap<QString, VariableDescriptor*>* CommandDescriptor::getVariableMap()
 VariableDescriptor* VariableDescriptor::FromJSON(QJsonObject qJsonObj, QString* errorString)
 {
     VariableDescriptor* vd = new VariableDescriptor();
-    
+
     if (! qJsonObj.contains("type"))
     {
         *errorString = "The variable had no \"type\" attribute.";
         return 0;
     }
-    
+
     if (! qJsonObj.value("type").isString())
     {
         *errorString = "The variable's \"type\" attribute was not a string.";
         return 0;
     }
-    
+
     if (! qJsonObj.contains("name"))
     {
         *errorString = "The variable had no \"name\" attribute.";
         return 0;
     }
-    
+
     if (! qJsonObj.value("name").isString())
     {
         *errorString = "The variable's \"name\" attribute was not a string.";
         return 0;
     }
-    
+
     if (! qJsonObj.contains("label"))
     {
         *errorString = "The variable had no \"label\" attribute.";
         return 0;
     }
-    
+
     if (! qJsonObj.value("label").isString())
     {
         *errorString = "The variable's \"label\" attribute was not a string.";
         return 0;
     }
-    
+
     QString type = qJsonObj.value("type").toString();
-    
+
     if (type == "string")
     {
         vd -> type = VariableDescriptor::TYPE_STRING;
@@ -135,13 +135,13 @@ VariableDescriptor* VariableDescriptor::FromJSON(QJsonObject qJsonObj, QString* 
             *errorString = "The variable is a multiple choice type variable, but has no \"choices\" array.";
             return 0;
         }
-        
+
         if (! qJsonObj.value("choices").isArray())
         {
             *errorString = "The variable is a multiple choice type variable, and the \"choices\" attribute is not an array.";
             return 0;
-        }        
-        
+        }
+
         vd -> type = VariableDescriptor::TYPE_MULTIPLE_CHOICE;
         vd -> defaultValue = "";
         vd -> choices = new QList<VariableDescriptor::MultipleChoiceItem>;
@@ -151,31 +151,31 @@ VariableDescriptor* VariableDescriptor::FromJSON(QJsonObject qJsonObj, QString* 
         for (int i = 0; i < choices.count(); i++)
         {
             choiceJsonObj = choices.at(i).toObject();
-            
+
             if (! choiceJsonObj.contains("value"))
             {
                 *errorString = "A member of the \"choices\" array has no \"value\" attribute.";
                 return 0;
             }
-            
+
             if (! choiceJsonObj.value("value").isString())
             {
                 *errorString = "A member of the \"choices\" array has a non-string \"value\" attribute.";
                 return 0;
             }
-            
+
             if (! choiceJsonObj.contains("label"))
             {
                 *errorString = "A member of the \"choices\" array has no \"label\" attribute.";
                 return 0;
             }
-            
+
             if (! choiceJsonObj.value("label").isString())
             {
                 *errorString = "A member of the \"choices\" array has a non-string \"label\" attribute.";
                 return 0;
             }
-            
+
             item.value = choiceJsonObj.value("value").toString();
             item.label  = choiceJsonObj.value("label").toString();
             vd -> choices -> append(item);
@@ -185,7 +185,7 @@ VariableDescriptor* VariableDescriptor::FromJSON(QJsonObject qJsonObj, QString* 
     {
         return 0;
     }
-    
+
     if (qJsonObj.contains("default"))
     {
         if (type == "boolean")
@@ -194,8 +194,8 @@ VariableDescriptor* VariableDescriptor::FromJSON(QJsonObject qJsonObj, QString* 
             {
                 *errorString = "The variable is a boolean variable, but has a non-boolean default value. The acceptable values are either \"true\" or \"false\", without quotes.";
                 return 0;
-            }            
-            
+            }
+
             if (qJsonObj.value("default").toBool())
             {
                 vd -> defaultValue = "true";
@@ -210,7 +210,7 @@ VariableDescriptor* VariableDescriptor::FromJSON(QJsonObject qJsonObj, QString* 
             }
             else
             {
-                vd -> defaultValue = qJsonObj.value("default").toString();    
+                vd -> defaultValue = qJsonObj.value("default").toString();
             }
         }
     }
