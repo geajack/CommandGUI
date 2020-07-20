6f3378bbec715f9da24d8b5a669dc498ce2de00e
Jack <->
2020-07-21 00:33:08 +0200

    Fix bug in core in which name is not loaded from JSON

--- src/ui/gtk/CommandPage.cpp
+++ src/ui/gtk/CommandPage.cpp
@@ -2,10 +2,14 @@
 
 CommandPage::CommandPage()
 {
-
+    label.set_text("Hello, world!");
+    add(label);
 }
 
 void CommandPage::loadCommandDescriptor(CommandDescriptor *descriptor)
 {
+    auto name = descriptor->name;
+    auto nVariables = descriptor->variableList->size();
 
+    label.set_text(name);
 }
