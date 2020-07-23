d8f914511b1c1b69f256a80298d051ca6bcdf100
Jack <->
2020-07-23 18:11:26 +0200

    Basic process running code

--- src/core/Process.cpp
+++ src/core/Process.cpp
@@ -1,15 +1,49 @@
 #include "Process.h"
 #include <ctime>
+#include <unistd.h>
 
 Process::Process(std::string command)
 {
     running = false;
+    this->command = command;
 }
 
-void Process::run()
+ProcessRunResult Process::run()
 {
     startTime = std::time(NULL);
     running = true;
+    output = "";
+
+    int pipeEndpoints[2];
+    pipe(pipeEndpoints);
+
+    unsigned int forkResult = fork();
+
+    if (forkResult == -1)
+    {
+        return FAILED;
+    }
+    else
+    {
+        if (forkResult > 0)
+        {
+            childPID = forkResult;
+            outputStream = pipeEndpoints[0];
+            close(pipeEndpoints[1]);
+            return OKAY;
+        }
+        else
+        {
+            close(pipeEndpoints[0]);
+            dup2(pipeEndpoints[1], STDOUT_FILENO);
+            dup2(pipeEndpoints[1], STDERR_FILENO);
+            //execlp("echo", "echo", "hello");
+            printf("%s", "hello world\n");
+            
+            exit(1);
+            return OKAY;
+        }
+    }
 }
 
 void Process::stop()
@@ -24,11 +58,26 @@ bool Process::isRunning()
 
 std::string Process::getOutput()
 {
-    std::string output = "";
-    int secondsElapsed = std::time(NULL) - startTime;
-    while (output.size() < secondsElapsed)
+    if (running)
     {
-        output += ".";
+        int result = read(outputStream, outputBuffer, BUFFER_SIZE - 1);
+        if (result == -1)
+        {
+            return "Failed to read stdout!";
+        }
+        else
+        {
+            if (result == 0)
+            {
+                running = false;
+            }
+            else
+            {
+                int bytesRead = result;
+                outputBuffer[bytesRead] = 0;
+                output += outputBuffer;
+            }
+        }
     }
     return output;
 }
--- src/core/Process.h
+++ src/core/Process.h
@@ -1,12 +1,25 @@
 #include <string>
 
+static const int BUFFER_SIZE = 1024;
+
+enum ProcessRunResult
+{
+    OKAY,
+    FAILED
+};
+
 class Process
 {
         int startTime;
         bool running;
+        std::string command;
+        int outputStream;
+        unsigned int childPID;
+        std::string output;
+        char outputBuffer[BUFFER_SIZE];
     public:
         Process(std::string command);
-        void run();
+        ProcessRunResult run();
         void stop();
         bool isRunning();
         std::string getOutput();
